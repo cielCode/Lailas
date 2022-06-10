950031b39284bfd2c8fac6615f9b8719d0d2a42c
HugLife1 <95499485+HugLife1@users.noreply.github.com>
2022-06-10 20:43:43 +0200

    Add project files.

---
+++ Lailas/Models/Chatt.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lailas.Models
+{
+    public class Chatt
+    {
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            public string Text { get; set; }
+       
+    }
+}
